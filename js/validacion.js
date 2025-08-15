4c6cf8c18166fbcac2a2a7c5e0de7925014018f6
Geronimo Silva <gerosso515@gmail.com>
2025-08-15 09:32:09 -0300

    Checkbox logic added

--- js/validacion.js
+++ js/validacion.js
@@ -1,7 +1,24 @@
 function showAlertSuccess() {
     document.getElementById("alert-success").classList.add("show");
+    setTimeout(function() {
+        document.getElementById("alert-success").classList.remove("show");
+    }, 3000); // Hide after 3 seconds
 }
 
 function showAlertError() {
     document.getElementById("alert-danger").classList.add("show");
+    setTimeout(function() {
+        document.getElementById("alert-danger").classList.remove("show");
+    }, 3000); // Hide after 3 seconds
+    
 }
+    
+
+document.getElementById("regBtn").addEventListener("click", function(event){
+    const checkbox = document.getElementById("terminos");
+    if (!checkbox.checked) {
+        event.preventDefault(); // Prevent form submission
+        showAlertError();
+        console.log("Checkbox is not checked");
+    }
+});
