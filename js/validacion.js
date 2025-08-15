23025cca8050d1a571d1c3858b186a84f0ba814d
LucasHenderson <hendersonlucas1818@gmail.com>
2025-08-15 09:49:52 -0300

    verifiaciones

--- js/validacion.js
+++ js/validacion.js
@@ -1,7 +1,53 @@
 function showAlertSuccess() {
-    document.getElementById("alert-success").classList.add("show");
+    const alert = document.getElementById("alert-success");
+    alert.classList.add("show")
+    setTimeout(() => {
+        alert.classList.remove("show")
+    }, 5000);
 }
 
 function showAlertError() {
-    document.getElementById("alert-danger").classList.add("show");
+    const alert = document.getElementById("alert-danger");
+    alert.classList.add("show")
+    setTimeout(() => {
+        alert.classList.remove("show")
+    }, 5000);
 }
+
+function verificarCamposVacios () {
+    const nombre = document.getElementById("nombre")
+    const apellido = document.getElementById("apellido")
+    const email = document.getElementById("email")
+    const pass1 = document.getElementById("password1")
+    const pass2 = document.getElementById("password2")
+
+    if (!nombre || !apellido || !email || !pass1 || !pass2)
+        return false
+    else
+        return true
+};
+
+function verificarContraValida() {
+    const pass1 = document.getElementById("password1").value
+    const pass2 = document.getElementById("password2").value
+
+    if (pass1.length >= 6 && pass1 === pass2)
+        return true
+    else
+        return false
+};
+
+const boton_registro = document.getElementById("regBtn")
+
+boton_registro.addEventListener("click", () => {
+    if (verificarCamposVacios() && verificarContraValida()){
+        console.log(verificarCamposVacios());
+        console.log(verificarContraValida());
+        showAlertSuccess();
+    }
+    else{
+        console.log(verificarCamposVacios());
+        console.log(verificarContraValida());
+        showAlertError();
+    }
+});
